f540c81d4bc81f2bb2e10b86d83dac7cea1fe1f0
laboufoul <laboufou@uncc.edu>
2021-04-26 16:03:26 -0400

    added more info to documentation

--- RSVPme/test_file.py
+++ RSVPme/test_file.py
@@ -1,7 +1,15 @@
 import unittest
 import requests
+from models import User
+from database import db
 
 class FlaskTest(unittest.TestCase):
+
+    # create mock user
+    mockUser = User(userName='username', email='email@email.com', hashedPassword='12345')
+    # Add this mock user object to the database
+    db.session.add(mockUser)
+    db.session.commit()
 
     def test_home(self):
         response = requests.get("http://127.0.0.1:5000/home")
@@ -25,7 +33,7 @@ class FlaskTest(unittest.TestCase):
         response = requests.get("http://127.0.0.1:5000/my_events")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-        self.assertEqual('My Events' in response.text, True)
+        self.assertEqual('Joined Events' in response.text, True)
 
     def test_create_event(self):
         response = requests.get("http://127.0.0.1:5000/events/create")
